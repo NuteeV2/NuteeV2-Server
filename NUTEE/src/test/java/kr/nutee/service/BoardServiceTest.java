a7d57b905bb00fadd6c3aa6b380c2ec65bdde4b3
choiyk <d_pvmf@naver.com>
2019-01-18 16:12:31 +0900

    chore: log 방법 변경
    Syestem.out.println -> LogBack

--- NUTEE/src/test/java/kr/nutee/service/BoardServiceTest.java
+++ NUTEE/src/test/java/kr/nutee/service/BoardServiceTest.java
@@ -6,18 +6,21 @@ import java.util.List;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
 
 import kr.nutee.dto.Board;
-import kr.nutee.exception.NonExistException;
 
 @SpringBootTest
 @RunWith(SpringRunner.class)
 @Transactional
 public class BoardServiceTest {
+
+	private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
 	@Autowired
 	BoardService boardService;
@@ -26,21 +29,14 @@ public class BoardServiceTest {
 	public void 전체게시판출력() {
 		List<Board> boards = boardService.findAll();
 		assertEquals(boards.size(), 7);
-		for(Board b : boards) {
-			System.out.println(b.toString());
-		}
+		logger.info(boards.toString());
 	}
 
 	@Test
 	public void 게시판하나출력() {
 		Board board = boardService.findOne(1);
 		assertEquals(board.getId(), 1);
-		System.out.println(board.toString());
-	}
-
-	@Test(expected=NonExistException.class)
-	public void 존재하지않는게시판() {
-		boardService.findOne(8);
+		logger.info(board.toString());
 	}
 
 }
