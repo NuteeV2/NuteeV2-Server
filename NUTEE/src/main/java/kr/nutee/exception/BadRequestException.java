8ce74b067e35fc6f9ce22ae9c7f45d93a9a69197
choiyk <d_pvmf@naver.com>
2019-01-18 16:06:37 +0900

    feat: Exception 이름 변경

---
+++ NUTEE/src/main/java/kr/nutee/exception/BadRequestException.java
@@ -0,0 +1,23 @@
+package kr.nutee.exception;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+/*
+ * 잘못된 url 요청이 있을 때 발생
+ *
+ * @author choiyk
+ */
+@ResponseStatus(value = HttpStatus.BAD_REQUEST, reason = "BadRequest")
+public class BadRequestException extends RuntimeException{
+
+	private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+	public BadRequestException(String message) {
+		super(message);
+		logger.info("BadRequestException: {}", message);
+	}
+
+}
