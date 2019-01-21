fc6abc3e08db9a080cac36d02260ff28e2f38416
choiyk <d_pvmf@naver.com>
2019-01-21 18:19:08 +0900

    test: 데이터 유효성 검사 Service layer로 옮김

--- NUTEE/src/test/java/kr/nutee/service/BoardServiceTest.java
+++ NUTEE/src/test/java/kr/nutee/service/BoardServiceTest.java
@@ -18,6 +18,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import kr.nutee.dto.Board;
+import kr.nutee.exception.InvalidDataException;
 import kr.nutee.model.BoardInsertRequestDto;
 import kr.nutee.repository.mapper.BoardMapper;
 import kr.nutee.service.impl.BoardServiceImpl;
@@ -62,7 +63,7 @@ public class BoardServiceTest {
 	}
 
 	@Test
-	public void 게시판삽입성공() {
+	public void 게시판삽입성공() throws InvalidDataException {
 		BoardInsertRequestDto boardDto = new BoardInsertRequestDto();
 		boardDto.setBoardName("게시판 삽입 테스트");
 		boardService.insert(boardDto);
