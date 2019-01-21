089fb3882f1f796b555940a6c349decde20ac31b
choiyk <d_pvmf@naver.com>
2019-01-21 11:06:45 +0900

    feat: 게시판 추가 구현

--- NUTEE/src/main/java/kr/nutee/controller/BoardController.java
+++ NUTEE/src/main/java/kr/nutee/controller/BoardController.java
@@ -2,13 +2,21 @@ package kr.nutee.controller;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import kr.nutee.dto.Board;
+import kr.nutee.exception.BadRequestException;
 import kr.nutee.service.impl.BoardServiceImpl;
 
 /*
@@ -17,8 +25,10 @@ import kr.nutee.service.impl.BoardServiceImpl;
  * @author choiyk
  */
 @RestController
-@RequestMapping("/boards")
+@RequestMapping("/api/boards")
 public class BoardController {
+
+	private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
 	private final BoardServiceImpl boardService;
 
@@ -33,9 +43,9 @@ public class BoardController {
 	 * @return 전체 게시판 List
 	 * @see /boards/
 	 */
-	@GetMapping("/")
-	public List<Board> boards(){
-		return boardService.findAll();
+	@GetMapping("")
+	public ResponseEntity<List<Board>> boards(){
+		return new ResponseEntity<List<Board>>(boardService.findAll(), HttpStatus.OK);
 	}
 
 	/*
@@ -45,8 +55,22 @@ public class BoardController {
 	 * @see /boards/{id}
 	 */
 	@GetMapping("{id}")
-	public Board board(@PathVariable("id") int id) {
-		return boardService.findOne(id);
+	public ResponseEntity<Board> board(@PathVariable("id") int id) {
+		Board board = boardService.findOne(id);
+		if(board == null) throw new BadRequestException("NonExist Board id "+id);
+		return new ResponseEntity<Board>(board, HttpStatus.OK);
+	}
+
+	@PostMapping("")
+	public ResponseEntity<String> insert(@RequestBody Board board){
+		try{
+			boardService.insert(board);
+			return new ResponseEntity<String>("", HttpStatus.OK);
+		}
+		catch(DataIntegrityViolationException e) {
+			logger.info("DataIntegrityViolationException: 데이터 삽입 오류");
+			return new ResponseEntity<String>("데이터 삽입 오류", HttpStatus.CONFLICT);
+		}
 	}
 
 }
