8c7bfaf077943b6747ad4e7f4fccc1906b748cfa
choiyk <d_pvmf@naver.com>
2019-01-22 13:51:28 +0900

    feat: MethodArgumentNotValidException을 위한 handler 작성
    parameter 값이 invalid한 경우 발생하는 MethodArgumentNotValidException을 위한 handler 작성

--- NUTEE/src/main/java/kr/nutee/exception/GlobalExceptionHandler.java
+++ NUTEE/src/main/java/kr/nutee/exception/GlobalExceptionHandler.java
@@ -6,6 +6,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,7 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import kr.nutee.model.ResponseBody;
 
 /*
- * Error 발생 시 ResponseEntity 형태로 반환하기 위한 Handler
+ * Error 발생 시 ResponseEntity 형태로 반환하기 위한 ExceptionHandler
  * @author choiyk
  */
 @ControllerAdvice
@@ -31,8 +32,21 @@ public class GlobalExceptionHandler {
 	@ExceptionHandler(value = BadRequestException.class)
 	public ResponseEntity<ResponseBody> handleBadRequestException(BadRequestException e) {
 		logger.info("BadRequestException: {}", e.getMessage());
-		ResponseBody message = new ResponseBody(new Date(), HttpStatus.BAD_REQUEST, e.getMessage());
-		return new ResponseEntity<ResponseBody>(message, HttpStatus.BAD_REQUEST);
+		ResponseBody body = new ResponseBody(new Date(), HttpStatus.BAD_REQUEST, e.getMessage());
+		return new ResponseEntity<ResponseBody>(body, HttpStatus.BAD_REQUEST);
+	}
+
+	/*
+	 * parameter 값이 invalid한 경우 발생하는 Exception을 위한 Handler
+	 * @param MethodArgumentNotValidException
+	 * @return ResponseEntity<ResponseBody>
+	 */
+	@ExceptionHandler(value = MethodArgumentNotValidException.class)
+	public ResponseEntity<ResponseBody> handleMethodArgumentNotValidException(MethodArgumentNotValidException e){
+		String errorMessage = e.getBindingResult().getAllErrors().get(0).getDefaultMessage();
+		logger.info("MethodArgumentNotValidException: {}", errorMessage);
+		ResponseBody body = new ResponseBody(new Date(), HttpStatus.UNPROCESSABLE_ENTITY, errorMessage);
+		return new ResponseEntity<ResponseBody>(body, HttpStatus.UNPROCESSABLE_ENTITY);
 	}
 
 }
