3f1b0dfede08d2457d4b839514bf1b144042aa83
choiyk <d_pvmf@naver.com>
2019-01-22 14:20:54 +0900

    feat: DataIntegrityViolationException Handler 작성

--- NUTEE/src/main/java/kr/nutee/exception/GlobalExceptionHandler.java
+++ NUTEE/src/main/java/kr/nutee/exception/GlobalExceptionHandler.java
@@ -1,9 +1,8 @@
 package kr.nutee.exception;
 
-import java.util.Date;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -11,7 +10,7 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
 
-import kr.nutee.model.ResponseBody;
+import kr.nutee.model.CustomResponseBody;
 
 /*
  * Error 발생 시 ResponseEntity 형태로 반환하기 위한 ExceptionHandler
@@ -30,10 +29,10 @@ public class GlobalExceptionHandler {
 	 * @return ResponseEntity<ResponseBody>
 	 */
 	@ExceptionHandler(value = BadRequestException.class)
-	public ResponseEntity<ResponseBody> handleBadRequestException(BadRequestException e) {
+	public ResponseEntity<CustomResponseBody> handleBadRequestException(BadRequestException e) {
 		logger.info("BadRequestException: {}", e.getMessage());
-		ResponseBody body = new ResponseBody(new Date(), HttpStatus.BAD_REQUEST, e.getMessage());
-		return new ResponseEntity<ResponseBody>(body, HttpStatus.BAD_REQUEST);
+		CustomResponseBody body = new CustomResponseBody(HttpStatus.BAD_REQUEST, e.getMessage());
+		return new ResponseEntity<CustomResponseBody>(body, HttpStatus.BAD_REQUEST);
 	}
 
 	/*
@@ -42,11 +41,24 @@ public class GlobalExceptionHandler {
 	 * @return ResponseEntity<ResponseBody>
 	 */
 	@ExceptionHandler(value = MethodArgumentNotValidException.class)
-	public ResponseEntity<ResponseBody> handleMethodArgumentNotValidException(MethodArgumentNotValidException e){
+	public ResponseEntity<CustomResponseBody> handleMethodArgumentNotValidException(MethodArgumentNotValidException e){
 		String errorMessage = e.getBindingResult().getAllErrors().get(0).getDefaultMessage();
 		logger.info("MethodArgumentNotValidException: {}", errorMessage);
-		ResponseBody body = new ResponseBody(new Date(), HttpStatus.UNPROCESSABLE_ENTITY, errorMessage);
-		return new ResponseEntity<ResponseBody>(body, HttpStatus.UNPROCESSABLE_ENTITY);
+		CustomResponseBody body = new CustomResponseBody(HttpStatus.UNPROCESSABLE_ENTITY, errorMessage);
+		return new ResponseEntity<CustomResponseBody>(body, HttpStatus.UNPROCESSABLE_ENTITY);
+	}
+
+	/*
+	 * DB insert나 update 시 키 제약에 위반할 경우 발생하는 Exception을 위한 Handler
+	 * @param DataIntegrityViolationException
+	 * @return ResponseEntity<ResponseBody>
+	 */
+	@ExceptionHandler(value = DataIntegrityViolationException.class)
+	public ResponseEntity<CustomResponseBody> handleDataIntegrityViolationException(DataIntegrityViolationException e){
+		String errorMessage = e.getMessage();
+		logger.info("DataIntegrityViolationException: {}", errorMessage);
+		CustomResponseBody body = new CustomResponseBody(HttpStatus.UNPROCESSABLE_ENTITY, errorMessage);
+		return new ResponseEntity<CustomResponseBody>(body, HttpStatus.UNPROCESSABLE_ENTITY);
 	}
 
 }
