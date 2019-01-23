a1f1f7dc394871f5dbc72ce739a972343c9cacef
choiyk <d_pvmf@naver.com>
2019-01-23 14:13:15 +0900

    feat: lombok 어노테이션 변경
    @Setter 삭제

--- NUTEE/src/main/java/kr/nutee/model/BoardInsertAndUpdateRequestDto.java
+++ NUTEE/src/main/java/kr/nutee/model/BoardInsertAndUpdateRequestDto.java
@@ -2,13 +2,17 @@ package kr.nutee.model;
 
 import javax.validation.constraints.NotBlank;
 
-import lombok.Data;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.ToString;
 
 /*
  * Board 추가 시 사용할 Dto
  * @author choiyk
  */
-@Data
+@Getter
+@AllArgsConstructor
+@ToString
 public class BoardInsertAndUpdateRequestDto {
 
 	@NotBlank(message="BoardName requires non blank value")
