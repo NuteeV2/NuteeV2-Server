4c78717fcb6b392919f1a2c43d53d0ca6b3e1f07
choiyk <d_pvmf@naver.com>
2019-02-07 02:16:03 +0900

    fix: lombok 어노테이션 수정
    update시 ibatis에서 객체 생성할 때 Setter를 쓰는 듯 함. Setter가 없으면 error 빌생하여 @Data로 변경

--- NUTEE/src/main/java/kr/nutee/model/CategoryUpdateRequestDto.java
+++ NUTEE/src/main/java/kr/nutee/model/CategoryUpdateRequestDto.java
@@ -2,17 +2,13 @@ package kr.nutee.model;
 
 import javax.validation.constraints.NotBlank;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.ToString;
+import lombok.Data;
 
 /*
  * 카테고리 수정 시 사용할 Dto
  * @author choiyk
  */
-@Getter
-@AllArgsConstructor
-@ToString
+@Data	//update시 ibatis에서 객체 생성할 때 Setter를 쓰는 듯 함. Setter가 없으면 error 빌생.
 public class CategoryUpdateRequestDto {
 
 	@NotBlank(message="Category name requires not blank value")
