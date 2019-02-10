ec10827f100f4ed095775cc1422b7a2827e1be1e
choiyk <d_pvmf@naver.com>
2019-02-10 17:20:03 +0900

    feat: 임시로 사용할 User Dto 작성

---
+++ NUTEE/src/main/java/kr/nutee/dto/User.java
@@ -0,0 +1,27 @@
+package kr.nutee.dto;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+/*
+ * User 테이블의 데이터를 담기 위한 DTO 클래스
+ *
+ * @author choiyk
+ */
+@Getter
+@NoArgsConstructor
+@ToString
+@EqualsAndHashCode
+public class User {
+
+	private int id;
+	private String nickname;
+	private String pw;
+	private String studentNumber;
+	private int roleId;
+	private int coution;
+	private String email;
+
+}
