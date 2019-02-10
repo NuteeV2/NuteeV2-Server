c965b9c4bfe9d2e535f18f00cdb33c38d7bfd317
choiyk <d_pvmf@naver.com>
2019-02-10 17:19:46 +0900

    feat: Article dto 작성

---
+++ NUTEE/src/main/java/kr/nutee/dto/Article.java
@@ -0,0 +1,31 @@
+package kr.nutee.dto;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+/*
+ * Article 테이블의 데이터를 담기 위한 DTO 클래스
+ *
+ * @author choiyk
+ */
+@Getter
+@NoArgsConstructor
+@ToString
+@EqualsAndHashCode
+public class Article {
+
+	private int id;
+	private String title;
+	private String contents;
+	private String dates;
+	private int userId;
+	private int hits;
+	private String anonymous;
+	private int categoryId;
+	private int boardId;
+	private String deleted;
+	private int report;
+
+}
