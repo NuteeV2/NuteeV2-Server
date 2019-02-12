3c27b27ae45f15b5f966a68d75819ffdbe7819af
choiyk <d_pvmf@naver.com>
2019-02-12 16:24:45 +0900

    feat: ArticleListResponseDto 수정

--- NUTEE/src/main/java/kr/nutee/model/Article/ArticleListResponseDto.java
+++ NUTEE/src/main/java/kr/nutee/model/Article/ArticleListResponseDto.java
@@ -1,20 +1,26 @@
 package kr.nutee.model.Article;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 /*
  * Article 전체 List 조회시 데이터를 담을 Dto
  * @author choiyk
  */
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class ArticleListResponseDto {
 
 	private int id;
 	private String title;
 	private String dates;
-	private int userId;	//익명의 게시글인 경우 스누피로 표기
+	private int userId;
 	private int categoryId;
 	private int hits;
 	private int report;
+	private String nickname;	//익명으로 작성된 게시글의 경우 '스누피'
+	private int empathy;
 
 }
