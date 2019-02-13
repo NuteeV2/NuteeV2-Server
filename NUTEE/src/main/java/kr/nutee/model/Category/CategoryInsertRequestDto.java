bbb1d057c53e7ad9923d76ef1b2f2171ed86bb67
choiyk <d_pvmf@naver.com>
2019-02-13 13:13:45 +0900

    feat: lombok 어노테이션 변경
    -> @Data

--- NUTEE/src/main/java/kr/nutee/model/Category/CategoryInsertRequestDto.java
+++ NUTEE/src/main/java/kr/nutee/model/Category/CategoryInsertRequestDto.java
@@ -3,23 +3,19 @@ package kr.nutee.model.Category;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.ToString;
+import lombok.Data;
 
 /*
  * 카테고리 추기 시 사용할 Dto
  * @author choiyk
  */
-@Getter
-@AllArgsConstructor
-@ToString
+@Data
 public class CategoryInsertRequestDto {
 
 	@NotBlank(message="CategoryName requires not blank value")
 	private String categoryName;
 
-	@NotNull(message="BoardId requires not null")
+	@NotNull(message="BoardId cannot be null")
 	private int boardId;
 
 }
