3cb61c148bb4b1bcf32badec9428927445581735
choiyk <d_pvmf@naver.com>
2019-03-08 00:26:46 +0900

    chore: file 저장 경로 설정

--- NUTEE/src/main/java/kr/nutee/NuteeApplication.java
+++ NUTEE/src/main/java/kr/nutee/NuteeApplication.java
@@ -2,8 +2,14 @@ package kr.nutee;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+
+import kr.nutee.property.FileStorageProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties({
+    FileStorageProperties.class
+})
 public class NuteeApplication {
 
 	public static void main(String[] args) {
